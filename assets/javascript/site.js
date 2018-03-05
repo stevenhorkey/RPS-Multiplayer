9162a3988621df6da63a351446474f8b1dcce59b
stevenhorkey <stevenhorkey@gmail.com>
2018-03-05 15:24:58 -0700

    progress

---
+++ assets/javascript/site.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD-DT8UQJg6UI5fn12E0oydonNL9G6Ot04",
+    authDomain: "rps-multiplayer-4daac-d072e.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-4daac-d072e.firebaseio.com",
+    projectId: "rps-multiplayer-4daac",
+    storageBucket: "",
+    messagingSenderId: "516661451569"
+  };
+  firebase.initializeApp(config);
