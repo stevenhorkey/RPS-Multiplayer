31ed93351727ab0385160a5c733d186d79ac961e
stevenhorkey <stevenhorkey@gmail.com>
2018-03-08 17:27:59 -0700

    progress

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -1,5 +1,4 @@
 // Initialize Firebase
-// Initialize Firebase
 var config = {
   apiKey: "AIzaSyD-DT8UQJg6UI5fn12E0oydonNL9G6Ot04",
   authDomain: "rps-multiplayer-4daac.firebaseapp.com",
@@ -20,37 +19,151 @@ var name2 = "";
 var wins2 = 0;
 var losses2 = 0;
 
+var turn;
+
+var game = {
+  functions : {
+    init: function(){
+
+      $('#name-submit').on("click", function() {
+        var name = $('#name-input').val().trim();    
+        if (name1 === ""){
+          name1 = name;
+          database.ref().set({
+            chat: "",
+            players: {
+              one : {
+                name:name1,
+                choice: '',                
+                wins:0,
+                losses:0
+              }
+            },
+          });
+        } else if (name2 === "") {
+          name2 = name;
+          $('#name-input').hide();
+          $('#name-submit').hide();
+          database.ref('players').update({
+              two : {
+                name:name2,
+                choice: '',
+                wins:0,
+                losses:0
+            },
+          });
+          database.ref().update({
+            turn : 1,
+          })
+    
+        }
+        $('#name-input').val("");
+      });
+    },
+    play : function(){
+      var rock = $('.rock')
+      rock.html('<i class="fas fa-hand-rock"></i>')
+      var paper = $('.paper')
+      paper.html('<i class="fas fa-hand-paper"></i>')
+      var scissors = $('.scissors')
+      scissors.html('<i class="fas fa-hand-scissors"></i>')
+      var choice1;
+      var choice2;
+      if (turn = 1){
+        $('.option').on('click', function(){
+          choice1 = $(this).attr('data-id');
+          database.ref('players').child('one').update({
+            choice: choice1
+          })
+          database.ref().update({
+            turn : 2
+          })
+        });
+      }
+      if(turn = 2){
+        choice2 = $(this).attr('data-id');
+          database.ref('players').child('two').update({
+            choice: choice2
+          })
+          database.ref().update({
+            turn : 1
+        })
+      }
+
+      
+
+    },
+    run : function(){
+      database.ref().on("value",function(snapshot){
+        if (snapshot.child('players').exists()){
+          console.log(snapshot.val().players.one.name)
+          name1 = snapshot.val().players.one.name;
+          wins1 = snapshot.val().players.one.wins;
+          losses1 = snapshot.val().players.one.losses;
+          $('.name-one').text(name1);
+          $('.win-loss-1').text("Wins: " + wins1 + " || Losses: " + losses1);
+        }
+        if (snapshot.child('players').child('two').exists()){
+          console.log(snapshot.val().players.two.name)          
+          name2 = snapshot.val().players.two.name;
+          wins2 = snapshot.val().players.two.wins;
+          losses2 = snapshot.val().players.two.losses;
+          $('.name-two').text(name2);
+          $('.win-loss-2').text("Wins: " + wins2 + " || Losses: " + losses2);
+          game.functions.play();
+          $('.player-inputs').hide();
+          turn = snapshot.val().turn;
+          console.log("turn: " + turn);
+          
+        }
+        
+      }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
+    },
+    chat : function(){
+      $('#chat-submit').on("click",function(){
+        var comment = $('.chat-input').val().trim();
+        console.log(comment)
+        if(comment !== ""){
+          database.ref('chat').push({
+            message : comment,
+          });
+        }
+      });
+      database.ref('chat').on("child_added",function(childSnapshot){
+        console.log(childSnapshot.val().message + "TEST")
+        var message = childSnapshot.val().message;
+        var row = $('<div></div>').text(message);
+        $('.chat-box').append(row)
+      });
+    
+      $('#clear-all').on("click",function(){
+        database.ref().set({});
+        location.reload();
+      });
+    }
+  }
+}
 
 $(document).ready(function(){
+  game.functions.run();
+  game.functions.init();
+  game.functions.chat();
+  console.log(database.ref('turn').turn);
+  // database.ref().update({
+  //   chat: "",
+  // })
 
-  $('#name-submit').on("click", function() {
-    var name = $('#name-input').val().trim();    
-    if (name1 === ""){
-      name1 = name;
-      console.log(name1);
-    } else {
-      name2 = name;
-      console.log(name2)
-    }
-    $('#name-input').val("");
-    database.ref().set({
-      player1: {
-        name:name1,
-        wins:0,
-        losses:0
-      },
-      player2: {
-        name:name2,
-        wins:0,
-        losses:0
-      }
-    });
-  });
+  
+  
+  
 
-  database.ref().on("value",function(snapshot){
 
-    
-  })
+
+
+
+
 })
 
 /* 
@@ -65,3 +178,4 @@ After 3 seconds, empty that winner display and adjust win count accordingly.
 CHAT:
 update the text box with what ever input is submitted from the text section and update firebase and display the result from firebase.
 */
+
